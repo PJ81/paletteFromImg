c76ec0a0cc5a141001998474a705fc51f1a3557b
PJ81 <paulo.jorente@web.de>
2023-06-12 14:12:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace paletteFromImg {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCreatePal = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnClip = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(687, 617);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(690, 3);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "load img";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCreatePal
+            // 
+            this.btnCreatePal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreatePal.Location = new System.Drawing.Point(690, 28);
+            this.btnCreatePal.Name = "btnCreatePal";
+            this.btnCreatePal.Size = new System.Drawing.Size(75, 23);
+            this.btnCreatePal.TabIndex = 2;
+            this.btnCreatePal.Text = "create pal";
+            this.btnCreatePal.UseVisualStyleBackColor = true;
+            this.btnCreatePal.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(774, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "20";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(773, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "min. color distance";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(690, 79);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(190, 535);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnClip
+            // 
+            this.btnClip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClip.Location = new System.Drawing.Point(690, 53);
+            this.btnClip.Name = "btnClip";
+            this.btnClip.Size = new System.Drawing.Size(190, 23);
+            this.btnClip.TabIndex = 6;
+            this.btnClip.Text = "copy to clipboard";
+            this.btnClip.UseVisualStyleBackColor = true;
+            this.btnClip.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 617);
+            this.Controls.Add(this.btnClip);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCreatePal);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Color Palette from Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCreatePal;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnClip;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace paletteFromImg {
+    public partial class Form1 : Form {
+
+        private List<Color> colors = new List<Color>();
+        Bitmap img;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            if(dlgOpen.ShowDialog()== DialogResult.OK) {
+                img = new Bitmap(dlgOpen.FileName);
+                pictureBox1.Image = img;
+            } 
+        }
+
+        private double dist(Color c1, Color c2) {
+            int r1 = c1.R, g1 = c1.G, b1 = c1.B,
+                r2 = c2.R, g2 = c2.G, b2 = c2.B;
+
+            int r = r1 - r2, g = g1 - g2, b = b1 - b2;
+            r *= r;
+            g *= g;
+            b *= b;
+
+            return Math.Sqrt(r + g + b);
+        }
+
+        private bool acceptColor(Color d) {
+            int ds = Convert.ToInt32(textBox1.Text);
+            if (ds < 1) {
+                ds = 1;
+                textBox1.Text = ds.ToString();
+            }
+
+            foreach (Color c in colors) {
+                if (dist(c, d) < ds) return false;
+            }
+            return true;
+        }
+
+        private Color getForeColor(Color c) {
+            float br2 = 0.3f * c.R + 0.59f * c.G + 0.11f * c.B;
+            if (br2 > 127.0) return Color.Black;
+            return Color.White;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            colors.Clear();
+            listView1.Items.Clear();
+
+            Cursor = Cursors.WaitCursor;
+            for (int y = 0; y < img.Height; y++) {
+                for(int x = 0; x < img.Width;x++) {
+                    Color c = img.GetPixel(x, y);
+                    if(acceptColor(c)) colors.Add(c);
+                }
+            }
+
+            Cursor = Cursors.Default;
+
+            foreach(Color c in colors) {
+                ListViewItem li = new ListViewItem();
+                li.BackColor = c;
+                li.ForeColor = getForeColor(c);
+                li.Text = "\n #" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2") + " " ;
+                listView1.Items.Add(li);
+            }
+
+            MessageBox.Show("Colors count: " + colors.Count.ToString());
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            string txt = "[";
+            foreach(ListViewItem li in listView1.Items) {
+                txt += "\"" + li.Text.Trim() + "\", "; 
+            }
+
+            Clipboard.SetText(txt.Substring(0, txt.Length - 2) + "]");
+        }
+    }
+}
